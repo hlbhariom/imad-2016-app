6ac5334e2ae4d3597fd385e2b8ed21bc47d73d03
hlbhariom <om.secadvice@gmail.com>
2016-09-29 23:14:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/:articleName', function (req, res) {  //':' symbol lets the articleNam
 
 //Javascript Response
 app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','main.js'));
+    res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
