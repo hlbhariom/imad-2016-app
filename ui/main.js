cdb6f21f1ebc90c26491c2ef7d91343ace702b93
hlbhariom <om.secadvice@gmail.com>
2016-09-29 23:40:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
+//Writes 'Loaded!' in console.
 console.log('Loaded!');
+
+
+//Change text of 'jstest-text' div
+var element = document.getElementById('jstest-text');
+element.innerHTML='This text has been changed using Javascript.';
+
+//Move Image
+var img=document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
