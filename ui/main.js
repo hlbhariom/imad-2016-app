bd38c974d642026b36bb4c6c9cbb721cce625deb
hlbhariom <om.secadvice@gmail.com>
2016-11-05 15:37:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,3 +36,11 @@ $(document).ready(function(){
         $("#panel").slideToggle("slow");
     });
 });
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
