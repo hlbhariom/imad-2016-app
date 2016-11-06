2abc3aac601934097433c826b3f2e4fbfe61fa39
Divya Rani <ranidivya063@gmail.com>
2016-11-06 07:50:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/login',function(req, res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/profile.css',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'profile.css'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
