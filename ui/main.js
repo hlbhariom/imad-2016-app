4b0d645c0295c973475faf3fecd6c9dc56d1aa36
Divya Rani <ranidivya063@gmail.com>
2016-11-07 05:38:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,113 +1,34 @@
- // Submit username/password to login
-var submit = document.getElementById('login_btn');
-submit.onclick = function () {
-// Create a request object
-var request = new XMLHttpRequest();
-// Capture the response and store it in a variable
-request.onreadystatechange = function () {
-if (request.readyState === XMLHttpRequest.DONE) {
-// Take some action
-if (request.status === 200) {
-submit.value = 'Sucess!';
-} else if (request.status === 403) {
-submit.value = 'Invalid credentials. Try again?';
-} else if (request.status === 500) {
-alert('Something went wrong on the server');
-submit.value = 'Login';
-} else {
-alert('Something went wrong on the server');
-submit.value = 'Login';
-}
-loadLogin();
-}
-// Not done yet
-};
-// Make the request
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', '/login/log', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username, password: password}));
-submit.value = 'Logging in...';
-};
+console.log('Loaded!');
 
-var register = document.getElementById('register_btn');
-register.onclick = function () {
-// Create a request object
-var request = new XMLHttpRequest();
-// Capture the response and store it in a variable
-request.onreadystatechange = function () {
-if (request.readyState === XMLHttpRequest.DONE) {
-// Take some action
-if (request.status === 200) {
-alert('User created successfully');
-register.value = 'Registered!';
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+var $this = $(this),
+label = $this.prev('label');
+if (e.type === 'keyup') {
+if ($this.val() === '') {
+label.removeClass('active highlight');
 } else {
-alert('Could not register the user');
-register.value = 'Register';
+label.addClass('active highlight');
 }
-}
-};
-// Make the request
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', '/signup/create-user', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username, password: password}));
-register.value = 'Registering...';
-};
-}
-function loadLoggedInUser (username) {
-var loginArea = document.getElementById('login_area');
-loginArea.innerHTML = `
-<h3> Hi <i>${username}</i></h3>
-<a href="/logout">Logout</a>
-`;
-}
-function loadLogin () {
-// Check if the user is already logged in
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-if (request.readyState === XMLHttpRequest.DONE) {
-if (request.status === 200) {
-loadLoggedInUser(this.responseText);
+} else if (e.type === 'blur') {
+if( $this.val() === '' ) {
+label.removeClass('active highlight');
 } else {
-loadLoginForm();
+label.removeClass('highlight');
+}
+} else if (e.type === 'focus') {
+if( $this.val() === '' ) {
+label.removeClass('highlight');
+}
+else if( $this.val() !== '' ) {
+label.addClass('highlight');
 }
 }
-};
-request.open('GET', '/check-login', true);
-request.send(null);
-}
-function loadArticles () {
-// Check if the user is already logged in
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-if (request.readyState === XMLHttpRequest.DONE) {
-var articles = document.getElementById('articles');
-if (request.status === 200) {
-var content = '<ul>';
-var articleData = JSON.parse(this.responseText);
-for (var i=0; i< articleData.length; i++) {
-content += `<li>
-<a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-(${articleData[i].date.split('T')[0]})</li>`;
-}
-content += "</ul>"
-articles.innerHTML = content;
-} else {
-articles.innerHTML('Oops! Could not load all articles!')
-}
-}
-};
-request.open('GET', '/get-articles', true);
-request.send(null);
-}
-// The first thing to do is to check if the user is logged in!
-loadLogin();
-// Now this is something that we could have directly done on the server-side using templating too!
-loadArticles();
+});
+$('.tab a').on('click', function (e) {
+e.preventDefault();
+$(this).parent().addClass('active');
+$(this).parent().siblings().removeClass('active');
+target = $(this).attr('href');
+$('.tab-content > div').not(target).hide();
+$(target).fadeIn(600);
+});
