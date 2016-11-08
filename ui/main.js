39449f6a07e255f5cf5ebd8ab830947fd47d3bd9
Divya Rani <ranidivya063@gmail.com>
2016-11-08 18:16:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+//counter code
+var counter=0;
+var button=getElementById('counter');
+button.onclick=function () {
+    //make a response
+    //caputre a response
+    //render a request
+    counter=counter+1;
+    var span=getElementById('count');
+    span.innerHTML=counter.toString();
+    
+};
