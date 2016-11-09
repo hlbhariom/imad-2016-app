5d34d6ef6d09674102cc43ff29f95b19658efbd6
Divya Rani <ranidivya063@gmail.com>
2016-11-09 16:11:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,13 @@
 console.log('Loaded!');
 //counter code
+var counter=0;
 
 var button=document.getElementById('counter');
 button.onclick=function () {
-    //create a response
-    var request = new XMLHttpRequest();
-    //caputre a response
-    request.onreadystatechange=function() {
-        if(request.readystatechange===XMLHttpRequest.Done){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=getElementById('count');
+    counter=counter+1;
+    var span=document.getElementById('count');
     span.innerHTML=counter.toString();
-            }
-        }
-            
-    };
-   //make the request
-   request.open('GET', 'http://divya063.imad.hasura-app.io/counter', true);
-   request.send(null);
-   
-    
-    
 };
+    //create a response
+   
+               
