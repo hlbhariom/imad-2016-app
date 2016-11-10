7907f341b1c13bc7fcf43d7a0441b60751ead7a2
Divya Rani <ranidivya063@gmail.com>
2016-11-10 16:52:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').pool;
+var Pool = require('pg').Pool;
+var config = {
+  
+  user: 'divya063',
+  database: 'divya063',
+  host: 'db.imad.hasura-app.io',
+  port:'5432',
+   password: process.env.DB_PASSWORD
+};
 var crypto=require('crypto');
 var bodyParser = require('body-parser');
 
@@ -14,7 +22,16 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
  
 });
 app.get('/co', function (req, res) {
