e2a3d4049b44cbe4b6b340da4c9e914183866d1e
Divya Rani <ranidivya063@gmail.com>
2016-11-12 20:19:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,11 +76,33 @@ var commentInput=document.getElementById('comment');
 var comment=commentInput.value;
 var submit=document.getElementById('comment_btn');
 submit.onclick=function() {
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+
-    }
+    var names=['name1','name2', 'name3'];
+ +     //create a response
+ +   var request=new XMLHttpRequest();
+ +   //capture the response
+ +   request.onreadystatechange=function(){
+ +       if(request.readyState===XMLHttpRequest.DONE){
+ +           if(request.status===200){
+ +                //capture response
+ +    var names=request.responseText;
+ +    names=JSON.parse(names);
+      var list='';
+      for(var i=0;i<names.length;i++){
+          list+='<li>'+names[i]+'</li>';
+      }
+      var ul=document.getElementById('namelist');
+      ul.innerHTML=list;
+      
+ +               
+ +           }
+ +           
+ +       }
+ +   };
+ +   //Make a request
+ +   var nameInput=document.getElementById('nam');
+ +var nam=nameInput.value;
+ +   request.open('GET','http://divya063.imad.hasura-app.io/submit-name?nam='+ nam,true);
+ +   request.send('null');
 }
     
    
