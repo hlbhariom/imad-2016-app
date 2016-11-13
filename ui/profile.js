706084cdb25d781c56d70ed9f86336e2947f0a35
Divya Rani <ranidivya063@gmail.com>
2016-11-13 07:44:36 +0530

    [imad-console] Updates ui/profile.js

--- ui/profile.js
+++ ui/profile.js
@@ -1,1 +1,35 @@
 
+window.onload = function(){
+document.getElementById('register_btn').onclick =function() {
+    
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          
+        }
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', 'http://divya063.imad.hasura-app.io/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username:username, password: password}));  
+        }; 
+    
+    };
+};
+
+    
