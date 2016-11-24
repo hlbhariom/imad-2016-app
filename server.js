f0f962aa6b8ca529eddc5f80317949a43e28acd8
hlbhariom <om.secadvice@gmail.com>
2016-11-25 01:06:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var config = {
     database: 'hlbhariom',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD;
+    password: process.env.DB_PASSWORD
 };
 var pool=new Pool(config);
 
